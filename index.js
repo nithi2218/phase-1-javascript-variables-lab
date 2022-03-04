89f2aa700eb21f001f2144d11d344e6c9f9eeb40
Nithyadevi <nithi2218@gmail.com>
2022-03-04 11:35:17 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber'; 
+
+function companyName() {
+    const companyName = scuber;
+}
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+function mostProfitableNeighborhood() {
+    mostProfitableNeighborhood = Chelsea;
+}
+
+let companyCeo = 'Susan Smith';
+function companyCeo() {
+   companyCeo = 'Susan Smith';
+}
+
+
+
+
+
+
+
